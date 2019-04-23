839e4a86bfacf1a954e508771c550c59671be3c5
Park Changwan <pcw109550@gmail.com>
2019-04-24 02:09:19 +0900

    CRT fault attack

---
+++ 15_Factoring_RSA_With_CRT_Optimization/solve.py
@@ -0,0 +1,28 @@
+from config import *
+from math import gcd
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
+
+
+fault = pow(y, e, n)
+p = gcd(fault - x, n)
+q = n // p
+assert(p * q == n)
+phin = (p - 1) * (q - 1)
+
+d = modinv(e, phin)
+
+print("{:d}".format(d % 1000000007))
